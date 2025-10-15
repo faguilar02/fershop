20c269cd34bbbc087ada18f8631a240268ef4c16
faguilar02 <daguilarv1@upao.edu.pe>
2025-10-15 10:46:42 -0500

    hacer que todos los usuarios tengna todos los permisos

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -51,6 +51,7 @@ export class AuthService {
         fullName,
         email,
         password,
+        roles: ['user', 'super', 'admin'], // Todos los usuarios se crean con todos los roles
       })
       .pipe(
         map((resp) => this.handleAuthSuccess(resp)),
